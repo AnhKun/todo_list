64e2f8580f329d071ca3bcbcf890302a67223c88
Anh Nguyen <nganhh014@gmail.com>
2021-12-22 16:33:35 +0200

    add profile part

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, url_for 
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
@@ -24,16 +24,45 @@ class TaskForm(FlaskForm):
     task = StringField('Your task', validators=[InputRequired()])
     submit = SubmitField('Save')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     form = TaskForm()
     tasks = Task.query.all()
 
-    if form.validate_on_submit():
-        new_task = Task(name=form.task.data, status='In progress')
-        db.session.add(new_task)
-        db.session.commit()
+    return render_template('index.html', tasks=tasks, form=form)
+
+@app.route('/add', methods=['POST'])
+def add():
+    form = TaskForm()
+
+    available_task = Task.query.filter_by(name=form.task.data).first()
+
+    if form.validate():
+        if available_task:
+            flash('This task alredy exist!')
+        else:
+            new_task = Task(name=form.task.data, status='In progress')
+            db.session.add(new_task)
+            db.session.commit()
 
         return redirect(url_for('index'))
 
-    return render_template('index.html', tasks=tasks, form=form)
+@app.route('/completed/<id>')
+def completed(id):
+    task = Task.query.filter_by(id=id).first()
+
+    task.status = 'Completed'
+    db.session.add(task)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=id).first()
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
